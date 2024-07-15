1b3dcc1da2fffea0c8fa9840a0fc12db1a19c292
Pierre-Marie <pierremariemarchio@gmail.com>
2024-07-15 15:40:48 +0200

    [feat] Database class implementation

--- public/index.php
+++ public/index.php
@@ -1,8 +1,14 @@
 <?php
 
+use Common\Core\Database;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 spl_autoload_register(function ($class){
     $result = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     require __DIR__ . "/../src/{$result}.php";
 });
+
+$config = require_once __DIR__ . '/../config/db.config.php';
+
+Database::getInstance($config['database']);
---
+++ src/Common/Core/Database.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Common\Core;
+
+use PDO;
+
+class Database
+{
+    private static Database | null $instance = null;
+    private PDO $connexion;
+    private mixed $statement;
+
+    private function __construct(array $config)
+    {
+
+        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['db']};charset={$config['charset']}";
+
+        $option = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        $this->connexion = $this->connexion($dsn, $config['user'], $config['pass'], $option);
+    }
+
+    // DATABASE INIT METHODS --------------------------------
+
+    public static function getInstance(array $config)
+    {
+        if (self::$instance === null) {
+            self::$instance = new self($config);
+        }
+
+        return self::$instance;
+    }
+
+    private function connexion(string $dns, string $user, string $pass, array $option)
+    {
+
+        try {
+            $pdo = new PDO($dns, $user, $pass, $option);
+        } catch (\PDOException $e) {
+            throw new \Exception($e->getMessage(), (int)$e->getCode());
+        }
+
+        return $pdo;
+    }
+
+    // DATABASE METHODS --------------------------------
+
+    private function fetch()
+    {
+        return $this->statement->fetch();
+    }
+
+    private function fetchAll()
+    {
+        $result = $this->statement->fetchAll();
+    }
+
+
+    public function fetchOrFail()
+    {
+        $result = $this->fetch();
+
+        if (!$result) {
+            // abort
+        }
+        // http status code
+        return $result;
+    }
+
+
+    public function fetchAllOrFail()
+    {
+        $result = $this->fetchAll();
+
+        if (!$result) {
+            // abort
+        }
+        // http status code
+        return $result;
+    }
+
+
+    public function query(string $query, array $params = [])
+    {
+        $this->statement = $this->connexion->prepare($query);
+        $this->statement->execute($params);
+    }
+}
