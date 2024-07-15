fd4c360bd52ec93434518b084f36d71b9129c3f2
Pierre-Marie <pierremariemarchio@gmail.com>
2024-07-15 11:58:28 +0200

    autoload

--- public/index.php
+++ public/index.php
@@ -1,1 +1,8 @@
 <?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+spl_autoload_register(function ($class){
+    $result = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    require __DIR__ . "/../src/{$result}.php";
+});
