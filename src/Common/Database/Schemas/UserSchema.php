3911d3a3e485d31212f0cf1888615dceec870dd3
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-07-16 11:34:25 +0200

    Add MangaSchema

--- src/Common/Database/Schemas/UserSchema.php
+++ src/Common/Database/Schemas/UserSchema.php
@@ -14,7 +14,7 @@ class UserSchema
 
         $db->getConnection();
         $db->query("CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
+            Id_user INT AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
             email VARCHAR(255) NOT NULL
         )");
