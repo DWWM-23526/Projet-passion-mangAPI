2610a6c7f8823d2a8eaa753ab0df7f4e0685c8db
Alex <delarobiaxana@gmail.com>
2024-07-17 14:30:46 +0200

    Tag repository

--- src/Tags/Repository/TagsRepository.php
+++ src/Tags/Repository/TagsRepository.php
@@ -10,21 +10,62 @@ class TagsRepository
 {
     private $db;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->db = App::inject()->getContainer(Database::class);
     }
 
-    public function getAll(){
+    public function getAll()
+    {
         $results = $this->db->query("SELECT * FROM tags")->fetchAll();
-        return array_map(fn ($data) => new Tags($data),$results);
+        return array_map(fn ($data) => new Tags($data), $results);
     }
 
-    public function getTagById(int $id){
-        $result = $this->db->query("SELECT * FROM tags WHERE Id_tag = :id",['id'=>$id]);
+    public function getTagById(int $id)
+    {
+        $result = $this->db->query("SELECT * FROM tags WHERE Id_tag = :id", ['id' => $id]);
         return $result ? new Tags($result) : null;
     }
 
-    public function createTag(Tags $tag){
-        
+    public function createTag(Tags $tag)
+    {
+        $sql = "INSERT INTO tags (
+        Id_tag, tag_name, is_deleted
+        ) VALUES (
+        :Id_tag, :tag_name, :is_deleted
+        )";
+
+        $values = $tag->toArray();
+
+        try {
+            $this->db->query($sql, $values);
+        } catch (\PDOException $e) {
+            throw new \Exception("Error on Tag creation" . $e->getMessage());
+        }
+    }
+
+    public function updateTag(Tags $tag)
+    {
+        $sql = "UPDATE tags
+                SET tag_name = :tag_name
+                    is_deleted = :is_deleted
+                WHERE Id_tag = :Id_tag";
+        $values = $tag->toArray();
+
+        try {
+            $this->db->query($sql, $values);
+        } catch (\PDOException $e) {
+            throw new \Exception("Error on Tag update" . $e->getMessage());
+        }
+    }
+
+    public function deleteTag(int $id)
+    {
+        $sql = "DELETE FROM tags WHERE Id_tag = :id";
+        try {
+            $this->db->query($sql, ['id' => $id]);
+        } catch (\PDOException $e) {
+            throw new \Exception("Error on Tag delete" . $e->getMessage());
+        }
     }
 }
