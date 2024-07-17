2be31c167760caaf39368b6bb1133ebc0518ddc1
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-07-17 21:41:00 +0200

    Ajout de la classe mère BaseMigration

--- src/Common/Database/migrations/AddIsDeletedToUsers.php
+++ src/Common/Database/migrations/AddIsDeletedToUsers.php
@@ -5,37 +5,17 @@ namespace Common\Database\Migrations;
 use Common\Core\App;
 use Common\Core\Database;
 
-class AddIsDeletedToUsers
+class AddIsDeletedToUsers extends BaseMigration
 {
   public function up()
   {
-    $db = App::inject()->getContainer(Database::class);
-    if ($db === null) {
-      throw new \Exception('Database connection could not be established.');
-    }
-
-    try {
-
-      $db->query("ALTER TABLE users ADD COLUMN is_deleted TINYINT(1) DEFAULT 0");
-    } catch (\Throwable $e) {
-
-      throw new \Exception("Error Processing Request :" . $e);
-    }
+    $query = "ALTER TABLE users ADD COLUMN is_deleted TINYINT(1) DEFAULT 0";
+    parent::upParent($query);
   }
 
   public function down()
   {
-    $db = App::inject()->getContainer(Database::class);
-    if ($db === null) {
-      throw new \Exception('Database connection could not be established.');
-    }
-
-    try {
-
-      $db->query("ALTER TABLE users DROP COLUMN is_deleted");
-    } catch (\Throwable $e) {
-
-      throw new \Exception("Error Processing Request :" . $e->getMessage());
-    }
+    $query = "ALTER TABLE users DROP COLUMN is_deleted";
+    parent::downParent($query);
   }
 }
