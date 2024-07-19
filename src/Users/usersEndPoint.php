939a99330d905d3e3ee15b8b80061116c1be3356
Alex <delarobiaxana@gmail.com>
2024-07-19 12:35:13 +0200

    Correction

--- src/Users/usersEndPoint.php
+++ src/Users/usersEndPoint.php
@@ -2,6 +2,17 @@
 
 use Common\Core\RequestMethod;
 
+// Get All
 $app->addRoute(RequestMethod::GET, '/api/users', 'Users\Controller\UsersController', 'getAllUsers');
-$app->addRoute(RequestMethod::GET, '/api/users/{user_id}', 'Users\Controller\UsersController', 'getAllUsers');
-$app->addRoute(RequestMethod::POST, '/api/users', 'Users\Controller\UsersController', 'getAllUsers');
+
+// Get User by ID
+$app->addRoute(RequestMethod::GET, '/api/users/{UserId}', 'Users\Controller\UsersController', 'getUserById');
+
+// Add User
+$app->addRoute(RequestMethod::POST, '/api/users', 'Users\Controller\UsersController', 'addUser');
+
+// Update User by ID
+$app->addRoute(RequestMethod::PUT, '/api/users/{UserId}', 'Users\Controller\UsersController', 'updateUser');
+
+// Delete User by ID
+$app->addRoute(RequestMethod::PUT, '/api/users/{UserId}', 'Users\Controller\UsersController', 'deleteUser');
