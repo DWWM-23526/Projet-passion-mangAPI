5d96e6a8f29dad35d67a34176355556509bda27a
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-07-22 09:17:43 +0200

    Add params to BaseRepository

--- src/Core/ORM/BaseRepository.php
+++ src/Core/ORM/BaseRepository.php
@@ -64,6 +64,14 @@ abstract class BaseRepository
 
     // ONE TO MANY
 
+    /**
+     * Summary of hasMany
+     * @param mixed $relatedClass
+     * @param string $relatedTable
+     * @param string $foreignKey
+     * @param int $localKeyId
+     * @return object[]
+     */
     protected function hasMany(mixed $relatedClass, string $relatedTable, string $foreignKey, int $localKeyId)
     {
 
@@ -73,6 +81,14 @@ abstract class BaseRepository
 
     // MANY TO ONE
 
+    /**
+     * Summary of belongTo
+     * @param mixed $relatedClass
+     * @param string $relatedTable
+     * @param string $foreignKey
+     * @param int $ownerKeyId
+     * @return object
+     */
     protected function belongTo(mixed $relatedClass, string $relatedTable, string $foreignKey, int $ownerKeyId)
     {
         $result = $this->db->query("SELECT * FROM {$relatedTable} WHERE {$foreignKey} = ?", [$ownerKeyId])->fetchOrFail();
@@ -81,18 +97,44 @@ abstract class BaseRepository
 
     // MANY TO MANY
 
+    /**
+     * Summary of belongToMany
+     * @param mixed $relatedClass
+     * @param string $relatedTable
+     * @param string $pivotTable
+     * @param string $relatedkey
+     * @param int $primaryKeyId
+     * @return object[]
+     */
     protected function belongToMany(mixed $relatedClass, string $relatedTable, string $pivotTable, string $relatedkey, int $primaryKeyId)
     {
         $result = $this->db->query("SELECT r.* FROM $relatedTable r JOIN $pivotTable p ON r.{$relatedkey} = p.{$relatedkey} WHERE p.{$this->primaryKey} = ?", [$primaryKeyId])->fetchAllOrFail();
         return array_map(fn ($data) => new $relatedClass($data), $result);
     }
 
+    /**
+     * Summary of attach
+     * @param string $pivotTable
+     * @param string $foreignKey
+     * @param string $relatedKey
+     * @param int $foreignkeyId
+     * @param int $relatedKeyId
+     * @return Database
+     */
     protected function attach(string $pivotTable, string $foreignKey, string $relatedKey, int $foreignkeyId, int $relatedKeyId)
     {
         return $this->db->query("INSERT INTO $pivotTable ({$foreignKey}, {$relatedKey}) VALUES (:foreignkeyId, :relatedKeyId)", [':foreignkeyId' => $foreignkeyId, ':relatedKeyId' => $relatedKeyId]);
     }
 
-
+    /**
+     * Summary of detach
+     * @param string $pivotTable
+     * @param string $foreignKey
+     * @param string $relatedKey
+     * @param int $foreignkeyId
+     * @param int $relatedKeyId
+     * @return Database
+     */
     protected function detach(string $pivotTable, string $foreignKey, string $relatedKey, int $foreignkeyId, int $relatedKeyId)
     {
         return $this->db->query("DELETE FROM $pivotTable WHERE {$foreignKey} = :foreignkeyId AND {$relatedKey} = :relatedKeyId", [':foreignkeyId' => $foreignkeyId, ':relatedKeyId' => $relatedKeyId]);
