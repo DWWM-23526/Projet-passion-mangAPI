dc22fa5ea3cf25e983bede1849b1f1df61b7146c
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-07-30 22:42:15 +0200

    hotfix

--- src/Api/EmailConfirm/emailConfirmEndPoint.php
+++ src/Api/EmailConfirm/emailConfirmEndPoint.php
@@ -6,11 +6,8 @@ use Core\RequestMethod;
 // GET emailConfirm by email
 $app->addRoute(RequestMethod::GET, "/api/emailConfirm/{email}", "Api\EmailConfirm\Controller\EmailConfirmController", "getEmailByEmail");
 
-// POST add a new emailConfirm into BDD
-// $app->addRoute(RequestMethod::POST, "/api/emailConfirm", "Api\EmailConfirm\Controller\EmailConfirmController", "addEmailConfirm");
-
 // POST send email
-$app->addRoute(RequestMethod::POST, "/api/emailConfirms", "Api\EmailConfirm\Controller\EmailConfirmController", "sendEmailToConfirmAccount");
+$app->addRoute(RequestMethod::POST, "/api/emailConfirm", "Api\EmailConfirm\Controller\EmailConfirmController", "sendEmailToConfirmAccount");
 
 //DELETE remove an emailConfirm
 $app->addRoute(RequestMethod::DELETE, "/api/emailConfirm/{email}", "Api\EmailConfirm\Controller\EmailConfirmController", "deleteEmailConfirm");
