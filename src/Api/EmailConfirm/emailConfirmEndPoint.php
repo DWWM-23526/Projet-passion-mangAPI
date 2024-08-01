bfca5d3f2af40f3a621e2b3a3a27d7e8408bb842
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-08-01 09:34:00 +0200

    modif name endpoint emailConfirm

--- src/Api/EmailConfirm/emailConfirmEndPoint.php
+++ src/Api/EmailConfirm/emailConfirmEndPoint.php
@@ -7,7 +7,7 @@ use Core\RequestMethod;
 $app->addRoute(RequestMethod::GET, "/api/emailConfirm/{email}", "Api\EmailConfirm\Controller\EmailConfirmController", "getEmailByEmail");
 
 // POST send email
-$app->addRoute(RequestMethod::POST, "/api/emailConfirm", "Api\EmailConfirm\Controller\EmailConfirmController", "sendEmailToConfirmAccount");
+$app->addRoute(RequestMethod::POST, "/api/sendEmailConfirm", "Api\EmailConfirm\Controller\EmailConfirmController", "sendEmailToConfirmAccount");
 
 //DELETE remove an emailConfirm
 $app->addRoute(RequestMethod::DELETE, "/api/emailConfirm/{email}", "Api\EmailConfirm\Controller\EmailConfirmController", "deleteEmailConfirm");
