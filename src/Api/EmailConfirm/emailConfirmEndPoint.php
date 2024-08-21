2f5565a3b7aaae32b8bb410729a91b0995ef1279
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-08-21 09:41:52 +0200

    modif endpoint mangaka and emailConfirm

--- src/Api/EmailConfirm/emailConfirmEndPoint.php
+++ src/Api/EmailConfirm/emailConfirmEndPoint.php
@@ -3,23 +3,16 @@ namespace Api\EmailConfirm;
 
 use Core\Base\BaseApiEndpoint;
 
-// // GET emailConfirm by email
-// $app->addRoute(RequestMethod::GET, "/api/emailConfirm/{email}", "Api\EmailConfirm\Controller\EmailConfirmController", "getEmailByEmail");
-
-// // POST send email
-// $app->addRoute(RequestMethod::POST, "/api/emailConfirm/sendEmailConfirm", "Api\EmailConfirm\Controller\EmailConfirmController", "sendEmailToConfirmAccount");
-
-// // GET token and decode
-// $app->addRoute(RequestMethod::GET, "/api/emailConfirm/{token}", "Api\EmailConfirm\Controller\EmailConfirmController", "decodeTokenAndCreateAccount");
-
-// //DELETE remove an emailConfirm
-// $app->addRoute(RequestMethod::DELETE, "/api/emailConfirm/{email}", "Api\EmailConfirm\Controller\EmailConfirmController", "deleteEmailConfirm");
-
 class EmailConfirmEndPoint extends BaseApiEndpoint
 {
-  public function __construct()
+  protected function getBasePath(): string
   {
-    parent::__construct('/api/emailConfirm', 'Api\EmailConfirm\Controller\EmailConfirmController');
+    return '/api/emailConfirm';
+  }
+
+  protected function getController(): string
+  {
+    return 'Api\EmailConfirm\Controller\EmailConfirmController';
   }
 
   protected function registerRoutes()
--- src/Api/Mangaka/mangakaEndPoint.php
+++ src/Api/Mangaka/mangakaEndPoint.php
@@ -1,38 +1,18 @@
 <?php
 namespace Api\Mangaka;
+
 use Core\Base\BaseApiEndpoint;
 
-// GET all mangakas
-
-// use Core\RequestMethod;
-
-// $app->addRoute(RequestMethod::GET, "/api/mangaka", "Api\Mangaka\Controller\MangakaController", "getAllMangakas");
-
-// // GET mangaka by Id
-// $app->addRoute(RequestMethod::GET, '/api/mangaka/{mangakaId}', 'Api\Mangaka\Controller\MangakaController', 'getMangakaById');
-
-// // GET search mangaka by name
-// $app->addRoute(RequestMethod::GET, '/api/mangaka/search/{searchTerm}', 'Api\Mangaka\Controller\MangakaController', 'searchMangakaByName');
-
-
-// // GET related mangas by mangaka Id
-// $app->addRoute(RequestMethod::GET, '/api/mangas/mangaka/{mangakaId}', 'Api\Mangaka\Controller\MangakaController', 'getAllRelatedManga');
-
-// // POST add a new manga
-
-// $app->addRoute(RequestMethod::POST, '/api/mangaka', 'Api\Mangaka\Controller\MangakaController', 'addMangaka')->middleware('auth');
-
-// // PUT update a manga
-
-// $app->addRoute(RequestMethod::PUT, '/api/mangaka/{mangakaId}', 'Api\Mangaka\Controller\MangakaController', 'updateMangaka')->middleware('auth');
-
-// // DELETE remove a manga
-// $app->addRoute(RequestMethod::DELETE, '/api/mangaka/{mangakaId}', 'Api\Mangaka\Controller\MangakaController', 'removeMangaka')->middleware('auth');
 class MangakaEndPoint extends BaseApiEndpoint
 {
-  public function __construct()
+  protected function getBasePath(): string
   {
-    parent::__construct('/api/mangaka', 'Api\Mangaka\Controller\MangakaController');
+    return '/api/mangaka';
+  }
+
+  protected function getController(): string
+  {
+    return 'Api\Mangaka\Controller\MangakaController';
   }
 
   protected function registerRoutes()
