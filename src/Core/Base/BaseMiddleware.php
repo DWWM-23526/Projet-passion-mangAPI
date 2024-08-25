1eb684c2a05ed5d9e0486f6ab367973b777d9d08
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2024-08-25 13:01:18 +0200

    Add adminMiddleware and update authEndpoint

--- src/Core/Base/BaseMiddleware.php
+++ src/Core/Base/BaseMiddleware.php
@@ -1,16 +1,16 @@
 <?php 
 namespace Core\Base;
 
+use middlewares\AdminMiddleware;
 use middlewares\AuthMiddleware;
 use middlewares\GuestMiddleware;
 
 class BaseMiddleware
 {
-
     const MAP = [
         'guest' => GuestMiddleware::class,
         'auth' => AuthMiddleware::class,
-
+        'admin' => AdminMiddleware::class,
     ];
 
 }
